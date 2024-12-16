a9fc9475ea055060f4514d6c13eaa13d3f70935c
MilaSem <milafoxsem@gmail.com>
2024-12-16 11:30:22 +0300

    refactor: add alt img description to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButton.alt='Delete';
     deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
